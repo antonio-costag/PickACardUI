58a000e6c7bd130b967d2a17204071ae9d8f1baa
Antonio Neto <antonio.neto.cost@gmail.com>
2025-04-02 14:17:14 -0300

    Finalização do projeto

--- PickACardUI/MainWindow.xaml.cs
+++ PickACardUI/MainWindow.xaml.cs
@@ -24,5 +24,17 @@ namespace PickACardUI
         {
             InitializeComponent();
         }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            //(int) converte o valor de numberOfCards.Value para um inteiro
+            string[] pickedCards = CardPicker.PickSomeCards((int)numberOfCards.Value);
+            listOfCards.Items.Clear();
+
+            foreach (string card in pickedCards)
+            {
+                listOfCards.Items.Add(card);
+            }
+        }
     }
 }
